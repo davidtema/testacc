c96bb1002a94a9d40b451044efe1f6a3271c9a13
adavydov <david.tema@yandex.ru>
2024-06-22 16:20:38 +0200

    add form labels and help

--- src/Application/Form/ApplicationType.php
+++ src/Application/Form/ApplicationType.php
@@ -7,6 +7,7 @@ namespace App\Application\Form;
 use App\Credit\Service\CreditFetcher;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,10 +15,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class ApplicationType extends AbstractType
 {
-    public function __construct(private readonly CreditFetcher $creditFetcher)
-    {
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -28,15 +25,27 @@ final class ApplicationType extends AbstractType
                     '3 месяца' => 3,
                     '6 месяцев' => 6,
                     '12 месяцев' => 12,
-                ]
+                ],
+                'label' => 'Длительность'
             ])
-            ->add('rate', TextType::class)
-            ->add('sum')
+            ->add('rate', TextType::class, [
+                'label' => 'Процентная ставка',
+            ])
+            ->add('sum', IntegerType::class, [
+                'label' => 'Сумма',
+            ])
             ->add('check', SubmitType::class, [
                 'label' => 'Проверить',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                    'style' => 'float: left; margin-right: 15px;'
+                ]
             ])
             ->add('apply', SubmitType::class, [
                 'label' => 'Выдать кредит',
+                'attr' => [
+                    'class' => 'btn btn-success',
+                ]
             ]);
     }
 
--- src/Client/Form/ClientType.php
+++ src/Client/Form/ClientType.php
@@ -8,7 +8,9 @@ use App\Address\Entity\Address;
 use App\Client\Entity\Client;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,9 +19,15 @@ final class ClientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('age')
+            ->add('name', TextType::class, [
+                'label' => 'Имя'
+            ])
+            ->add('surname', TextType::class, [
+                'label' => 'Фамилия'
+            ])
+            ->add('age', TextType::class, [
+                'label' => 'Возраст'
+            ])
             ->add('address', EntityType::class, [
                 'class' => Address::class,
                 'choice_label' => fn(Address $address) => $address->getZip() . ', ' . $address->getState() . ', ' . $address->getTown(),
@@ -27,11 +35,20 @@ final class ClientType extends AbstractType
                 'required' => true,
                 'label' => 'Адрес',
             ])
-            ->add('income')
-            ->add('ssn')
-            ->add('fico')
+            ->add('income', TextType::class, [
+                'label' => 'Доход в месяц (доллар США)'
+            ])
+            ->add('ssn', TextType::class, [
+                'label' => 'SSN (социальный страховой номер)'
+            ])
+            ->add('fico', IntegerType::class, [
+                'label' => 'FICO (кредитный рейтинг)',
+                'help' => 'Значение от 300 до 850'
+            ])
             ->add('email')
-            ->add('phone')
+            ->add('phone', TextType::class, [
+                'label' => 'Телефон'
+            ])
             ->add('save', SubmitType::class, [
                 'row_attr' => ['class' => 'mt-4']
             ]);
